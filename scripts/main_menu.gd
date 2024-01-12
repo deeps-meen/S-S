e41fc87d3a7a2bd4cf15d8fad8085e27ff24068f
deeps-meen <86970987+deeps-meen@users.noreply.github.com>
2024-01-12 11:38:03 +0530

    main_menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,25 @@
+extends ParallaxBackground
+
+@onready var clouds = $clouds
+@onready var clouds_2 = $clouds2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	clouds.motion_offset+= Vector2(20.0,0)*delta
+	clouds_2.motion_offset-= Vector2(5.0,0)*delta
+
+func _unhandled_key_input(event):
+	if event.is_action("ui_cancel"):
+		get_tree().quit()
+
+
+func _on_start_button_up():
+	pass # Replace with function body.
+
+
+func _on_exit_button_down():
+	get_tree().quit()
